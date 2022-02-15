fd77e08c3a3a0d586b57293929280b3d7246bbf6
samarthtehri <samarth252002@gmail.com>
2022-02-15 09:31:10 -0500

    V0.5 distribution of tokens

--- main.js
+++ main.js
@@ -42,3 +42,49 @@ function tokenLaunch() {
     })
 }
 
+// Sending 2000 tokens to a test user
+const amountToSend = 2000
+const newUser = new BigchainDB.Ed25519Keypair(bip39.mnemonicToSeed('newUserseedPhrase').slice(0, 32))
+
+// Actual function that transfers the tokens
+function transferTokens() {
+    // User who will receive the 2000 tokens
+    const newUser = new BigchainDB.Ed25519Keypair()
+
+    // Search outputs of the transactions belonging the token creator
+    // False argument to retrieve unspent outputs
+    conn.getTransaction(createTxId).then((txOutputs) => {
+            // Create transfer transaction
+            const createTranfer = BigchainDB.Transaction.makeTransferTransaction(
+                [{
+                    tx: txOutputs,
+                    output_index: 0
+                }],
+                // Transaction output: Two outputs, because the whole input
+                // must be spent
+                [BigchainDB.Transaction.makeOutput(
+                        BigchainDB.Transaction.makeEd25519Condition(tokenCreator.publicKey),
+                        (tokensLeft - amountToSend).toString()),
+                    BigchainDB.Transaction.makeOutput(
+                        BigchainDB.Transaction.makeEd25519Condition(newUser.publicKey),
+                        amountToSend)
+                ],
+                // Metadata (optional)
+                {
+                    transfer_to: 'john',
+                    tokens_left: tokensLeft
+                }
+            )
+
+            // Sign the transaction with the tokenCreator key
+            const signedTransfer = BigchainDB.Transaction.signTransaction(createTranfer, tokenCreator.privateKey)
+            return conn.postTransactionCommit(signedTransfer)
+        }).then(res => {
+            // Update tokensLeft
+            tokensLeft -= amountToSend
+            document.body.innerHTML += '<h3>Transfer transaction created</h3>'
+            document.body.innerHTML += res.id
+        })
+
+}
+
